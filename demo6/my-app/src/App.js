ab280728be40376cbe0d920bf211dfab839f2299
Shisham Chudal <shishamchudal61@gmail.com>
2025-09-03 15:43:37 -0700

    Updated the button!

--- demo6/my-app/src/App.js
+++ demo6/my-app/src/App.js
@@ -11,14 +11,13 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="btn btn-dange"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
-        <Button color="danger">Danger!</Button>
       </header>
     </div>
   );
