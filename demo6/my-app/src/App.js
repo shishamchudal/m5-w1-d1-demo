e5e3dbd3133a98060115236b06ab68e9c52c528e
Shisham Chudal <shishamchudal61@gmail.com>
2025-09-03 15:44:56 -0700

    fixed the class name!

--- demo6/my-app/src/App.js
+++ demo6/my-app/src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="btn btn-dange"
+          className="btn btn-danger"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
